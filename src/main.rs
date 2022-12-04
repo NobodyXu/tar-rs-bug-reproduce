96a3db201d190f0da93642e8e0f40a71e8ca7f88
Jiahao XU <Jiahao_XU@outlook.com>
2022-12-04 18:56:26 +1100

    Test tokio-tar instead of tar
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use flate2::read::GzDecoder;
-use std::fs::File;
-use tar::Archive;
+use async_compression::tokio::bufread::GzipDecoder;
+use tokio::{fs::File, io::BufReader};
+use tokio_tar::Archive;
 
-fn main() {
-    let f = File::open("cbindgen-0.24.3-x86_64-apple-darwin.tar.gz").unwrap();
-    let tar = GzDecoder::new(f);
-    Archive::new(tar).unpack(".").unwrap();
+#[tokio::main]
+async fn main() {
+    let f = File::open("cbindgen-0.24.3-x86_64-apple-darwin.tar.gz")
+        .await
+        .unwrap();
+    let tar = GzipDecoder::new(BufReader::new(f));
+    Archive::new(tar).unpack(".").await.unwrap();
 }
